b157a54a6e727033f46ad17b5512258482301ec4
Matthew Taylor <rhyolight@gmail.com>
2010-02-11 14:28:37 -0800

    Initial push into YUI3 Gallery fork

---
+++ src/gallery-formbinder/js/FormBind.js
@@ -0,0 +1,124 @@
+YUI().add('FormBind', function(Y) {
+    var FORMBIND_NAME = 'FormBind',
+        FORMBIND_NAMESPACE = 'forms',
+        tagBindings = null;
+        
+        tagBindings = {
+            input: function(target, name, value) {
+                target.set('value', value);
+            },
+            textarea: function(target, name, value) {
+                target.set('value', value);
+            },
+            select: function(target, name, value) {
+                var field = target.one('option[value=' + value +']');
+                if (!field) {
+                    throw new Error('Cannot bind value "' + value + '" to a combo box without that option available.');
+                }
+                field.set('selected', true);
+            },
+            fieldset: function(target, name, value, data, form) {
+                // attempt to bind to a group of radio buttons, but if that fails then try other stuff
+                if (!bindRadio(name, value, form)) {
+                    // if the data is an array, they represent checkboxes that should be checked
+                    if (value instanceof Array) {
+                        bindCheckboxes(value, form);
+                    }
+                }
+            }
+        };
+    
+    function bindRadio(name, value, form) {
+        var input = form.one('input[type="radio"]#' + name + '-' + value);
+        if (input) {
+            input.set('checked', true);
+            return true;
+        } 
+        return false;
+    }
+    
+    function bindCheckboxes(data, form) {
+        var input = form.all('input[type="checkbox"]')
+        if (input.size() == 0) return false;
+        Y.each(input, function(checkboxElement, i) {
+            for (var cbVal in data) {
+                if (checkboxElement.get('id') == data[cbVal]) {
+                    checkboxElement.set('checked', true);
+                }
+            }
+        });
+    }
+    
+    function dateToObj(d) {
+        return {year: d.getFullYear(), month: d.getMonth()+1, day: d.getDate()};
+    }
+    
+    function handleItemGroup(g, label, formId, form) {
+        var detailObjLabel = null,  // used to find out if we are inside a detail object
+        sublabel = '';              // used to drill down into sub objects of a group
+        // handle each attribute
+        for (prop in g) {
+            var value = g[prop];
+            // if this is a date object, we want to break it up and resubmit
+            if (value instanceof Date) {
+                this.formBind({label: prop, data:dateToObj(value)}, formId);
+            } else {
+                if (detailObjLabel) {
+                    sublabel = detailObjLabel;
+                } else {
+                    sublabel = label.length > 0 ? label + '-' + prop : prop;
+                    if (sublabel == 'label') {
+                        detailObjLabel = g[prop];
+                        continue;
+                    }
+                }
+            }
+            target = form.one('#' + sublabel);
+            // if there is an element with an id matching this data key name
+            if (target) {
+                tagBindings[target.get('tagName').toLowerCase()](target, sublabel, value, g, form);
+            }
+        }
+    }
+    
+    Y.namespace(FORMBIND_NAMESPACE);
+    
+    Y[FORMBIND_NAMESPACE][FORMBIND_NAME] = {
+        
+        formBind: function(data, formId) {
+            var target = null,  // element in form we're targeting 
+                prop = null,    // a name of a data in the data object 
+                label = '',     // possible label of the data object
+                i = 0,          // to iterate through arrays
+                form = Y.one('#' + formId);
+
+            // uncheck any checkboxes in the form
+            form.all('input[type="checkbox"]').set('checked', false);
+
+            // if there is a label, that means there is a map of data associated with this form object,
+            // so we'll take out the label and extract the data
+            if (data.label) {
+                label = data.label;
+                data = data.data;
+            }
+            
+            // if this data contains a date and format string, we'll need to process it differently
+            if (data.date && data.format) {
+                var d = new Date(Date.parse(data.date, data.format));
+                return this.formBind({label: label, data:dateToObj(d)}, formId);
+            }
+            
+            if (data instanceof Array) {
+                for (i = 0; i < data.length; i++) {
+                    handleItemGroup.call(this, data[i], label, formId, form);
+                }
+            }
+            
+            handleItemGroup.call(this, data, label, formId, form);
+
+        }
+        
+    };
+    
+    return Y[FORMBIND_NAMESPACE][FORMBIND_NAME];
+}, '0.1', { requires: ['node'] });
---
+++ src/gallery-formbinder/tests/FormBindTests.js
@@ -0,0 +1,264 @@
+YUI.add("form_binding_tests", function(Y) {
+    var assert = Y.Assert;
+             
+    //-------------------------------------------------------------------------
+    // Test Suite 
+    //-------------------------------------------------------------------------
+
+    var testSuite = new Y.Test.Suite({
+        name: "Form Binding Tests"
+    });
+    
+    testSuite.add(new Y.Test.Case({
+        
+        name:'Basic Tests',
+        
+        _should: {
+            error: {
+                testBindDate_InMapFormat_ThrowsErrorWhenComboboxSelectionDoesntExist: 'Cannot bind value "41" to a combo box without that option available.'
+            }
+        },
+        
+        // stores the clean empty form on the first test run
+        setUp: function() {
+            if (!this.cleanFormMarkup) {
+                this.cleanFormMarkup = Y.one('#formWrapper').get('innerHTML');
+            }
+        },
+        
+        // replaces the dirty form with the clean one
+        tearDown: function() {
+            Y.one('#formWrapper').set('innerHTML', this.cleanFormMarkup);
+        },
+        
+        testBindTextInput: function(){
+            var d = {fname: 'Matthew', lname: 'Taylor'};
+            
+            Y.forms.FormBind.formBind(d, 'personal-info');
+            
+            var fname = Y.one('input#fname').get('value');
+            assert.isNotNull(fname, 'first name not entered');
+            assert.areEqual('Matthew', fname, 'first name was wrong');
+            var lname = Y.one('input#lname').get('value');
+            assert.isNotNull(lname, 'last name not entered');
+            assert.areEqual('Taylor', lname, 'last name was wrong');
+        },
+        
+        testBindTextInput_InDetailObject: function() {
+            var d = [
+                {label:'fname', data:'Matthew'}, {label:'lname', data:'Taylor'}
+            ];
+            
+            Y.forms.FormBind.formBind(d, 'personal-info');
+            
+            var fname = Y.one('input#fname').get('value');
+            assert.isNotNull(fname, 'first name not entered');
+            assert.areEqual('Matthew', fname, 'first name was wrong');
+            var lname = Y.one('input#lname').get('value');
+            assert.isNotNull(lname, 'last name not entered');
+            assert.areEqual('Taylor', lname, 'last name was wrong');
+        },
+        
+        testBindRadioInput: function() {
+            var d = {gender: 'male'};
+            
+            Y.forms.FormBind.formBind(d, 'personal-info');
+            
+            assert.isTrue(Y.one('input#gender-male').get('checked'), 'male radio not checked');
+            assert.isFalse(Y.one('input#gender-female').get('checked'), 'female radio was checked');
+        }, 
+
+        testBindRadioInput_InDetailObject: function() {
+            var d = [{label: 'gender', data:'male'}];
+            
+            Y.forms.FormBind.formBind(d, 'personal-info');
+            
+            assert.isTrue(Y.one('input#gender-male').get('checked'), 'male radio not checked');
+            assert.isFalse(Y.one('input#gender-female').get('checked'), 'female radio was checked');
+        },
+        
+        testBindCheckbox: function() {
+            var d = {'fav-sammiches': ['cheese', 'ham', 'rueben']};
+            
+            Y.forms.FormBind.formBind(d, 'personal-info');
+            
+            assert.isTrue(Y.one('input#cheese').get('checked'), 'cheese was not checked');
+            assert.isFalse(Y.one('input#egg').get('checked'), 'egg was checked');
+            assert.isFalse(Y.one('input#chicken').get('checked'), 'chicken was checked');
+            assert.isTrue(Y.one('input#rueben').get('checked'), 'rueben was not checked');
+            assert.isTrue(Y.one('input#ham').get('checked'), 'ham was not checked');
+        },
+        
+        testBindCheckbox_InDetailObject: function() {
+            var d = [{label: 'fav-sammiches', data: ['cheese', 'ham', 'rueben']}];
+            
+            Y.forms.FormBind.formBind(d, 'personal-info');
+            
+            assert.isTrue(Y.one('input#cheese').get('checked'), 'cheese was not checked');
+            assert.isFalse(Y.one('input#egg').get('checked'), 'egg was checked');
+            assert.isFalse(Y.one('input#chicken').get('checked'), 'chicken was checked');
+            assert.isTrue(Y.one('input#rueben').get('checked'), 'rueben was not checked');
+            assert.isTrue(Y.one('input#ham').get('checked'), 'ham was not checked');
+        },
+        
+        testBindCheckbox_WhenSomeCheckboxesAreAlreadyChecked : function() {
+            // check all boxes
+            Y.all('input[type=checkbox]').each(function(ch) {
+                ch.set('checked', true);
+            });
+            var d = {'fav-sammiches': ['cheese', 'ham', 'rueben']};
+            
+            Y.forms.FormBind.formBind(d, 'personal-info');
+            
+            assert.isTrue(Y.one('input#cheese').get('checked'), 'cheese was not checked');
+            assert.isFalse(Y.one('input#egg').get('checked'), 'egg was checked');
+            assert.isFalse(Y.one('input#chicken').get('checked'), 'chicken was checked');
+            assert.isTrue(Y.one('input#rueben').get('checked'), 'rueben was not checked');
+            assert.isTrue(Y.one('input#ham').get('checked'), 'ham was not checked');        },
+        
+        testBindDate_InMapFormat_MonthAndDayAsComboboxes_YearAsTextInput: function() {
+            var i = 0, d = {label:'birth', data:{year:1978, month:7, day:11}};
+
+            Y.forms.FormBind.formBind(d, 'personal-info');
+
+            // month
+            var monthOptions = Y.all('select#birth-month option');
+            monthOptions.each(function(monthOption) {
+                if (monthOption.get('value') == d.data.month) {
+                    assert.isTrue(monthOption.get('selected'), 'because month is ' + d.data.month + ', month option with value of ' + monthOption.get('value') + ' should be selected');
+                } else {
+                    assert.isFalse(monthOption.get('selected'), 'because month is ' + d.data.month + ', month option with value of ' + monthOption.get('value') + ' should not be selected');
+                }
+            });
+            // day
+            var dayOptions = Y.all('select#birth-day option');
+            dayOptions.each(function(dayOption) {
+                if (dayOption.get('value') == d.data.day) {
+                    assert.isTrue(dayOption.get('selected'), 'because day is ' + d.data.day + ', day option with value of ' + dayOption.get('value') + ' should be selected');
+                } else {
+                    assert.isFalse(dayOption.get('selected'), 'because day is ' + d.data.day + ', day option with value of ' + dayOption.get('value') + ' should not be selected');
+                }
+            });
+            // year
+            assert.areEqual('1978', Y.one('input#birth-year').get('value'), 'birth year was not correct');
+        },
+        
+        testBindDate_InMapFormat_MonthAndDayAreText_AndYearIsComboBox: function() {
+            // first replace the month and day combos with text inputs
+            Y.one('select#birth-month').remove()
+            Y.one('select#birth-day').remove()
+            var wrapperDiv = Y.one('#birth div');
+            wrapperDiv.append(Y.Node.create('<input type="text" id="birth-month" name="birth-month"/>'));
+            wrapperDiv.append(Y.Node.create('<input type="text" id="birth-day" name="birth-day"/>'));
+            // now replace the year text input with a select with options
+            Y.one('input#birth-year').remove();
+            var select = Y.Node.create('<select id="birth-year" name="birth-year"></select>');
+            for (var i=1950; i<2000; i++) {
+                select.append(Y.Node.create('<option value="' + i + '">'+ i + '</option>'));
+            }
+            wrapperDiv.append(select);
+
+            var i = 0, d = {label:'birth', data:{year:1978, month:7, day:11}};
+
+            Y.forms.FormBind.formBind(d, 'personal-info');
+            
+            // month
+            assert.areEqual('7', Y.one('input#birth-month').get('value'), 'birth month was not correct');
+            // day
+            assert.areEqual('11', Y.one('input#birth-day').get('value'), 'birth day was not correct');
+            // year
+            var yearOptions = Y.all('select#birth-year option');
+            yearOptions.each(function(yearOption) {
+                if (yearOption.get('value') == d.data.year) {
+                    assert.isTrue(yearOption.get('selected'), 'because year is ' + d.data.year + ', year option with value of ' + yearOption.get('value') + ' should be selected');
+                } else {
+                    assert.isFalse(yearOption.get('selected'), 'because year is ' + d.data.year + ', year option with value of ' + yearOption.get('value') + ' should not be selected');
+                }
+            });
+        },
+        
+        // this test is configured to throw a specific error, see this._should.error for details (*vomit*)
+        testBindDate_InMapFormat_ThrowsErrorWhenComboboxSelectionDoesntExist: function() {
+            var d = {label:'birth', data:{year:1978, month:7, day:41}};
+
+            Y.forms.FormBind.formBind(d, 'personal-info');
+        },
+        
+        testBindDateObject: function() {
+            var i = 0, d = {birth: new Date(1978, 6, 11, 0, 0, 0, 0)};
+
+            Y.forms.FormBind.formBind(d, 'personal-info');
+
+            // month
+            var monthOptions = Y.all('select#birth-month option');
+            var month = d.birth.getMonth() + 1, day = d.birth.getDate(), year = d.birth.getFullYear();
+            monthOptions.each(function(monthOption) {
+                if (monthOption.get('value') == month) {
+                    assert.isTrue(monthOption.get('selected'), 'because month is ' + month + ', month option with value of ' + monthOption.get('value') + ' should be selected');
+                } else {
+                    assert.isFalse(monthOption.get('selected'), 'because month is ' + month + ', month option with value of ' + monthOption.get('value') + ' should not be selected');
+                }
+            });
+            // day
+            var dayOptions = Y.all('select#birth-day option');
+            dayOptions.each(function(dayOption) {
+                if (dayOption.get('value') == day) {
+                    assert.isTrue(dayOption.get('selected'), 'because day is ' + day + ', day option with value of ' + dayOption.get('value') + ' should be selected');
+                } else {
+                    assert.isFalse(dayOption.get('selected'), 'because day is ' + day + ', day option with value of ' + dayOption.get('value') + ' should not be selected');
+                }
+            });
+            // year
+            assert.areEqual('1978', Y.one('input#birth-year').get('value'), 'birth year was not correct');
+        },
+        
+        testBindDataString: function() {
+            var i = 0, d = {label:'birth', data:{date:'1978/07/11', format: '%Y/%b/%d'}};
+
+            Y.forms.FormBind.formBind(d, 'personal-info');
+
+            // month
+            var monthOptions = Y.all('select#birth-month option');
+            var month = '7', day = '11', year = '1978';
+            monthOptions.each(function(monthOption) {
+                if (monthOption.get('value') == month) {
+                    assert.isTrue(monthOption.get('selected'), 'because month is ' + month + ', month option with value of ' + monthOption.get('value') + ' should be selected');
+                } else {
+                    assert.isFalse(monthOption.get('selected'), 'because month is ' + month + ', month option with value of ' + monthOption.get('value') + ' should not be selected');
+                }
+            });
+            // day
+            var dayOptions = Y.all('select#birth-day option');
+            dayOptions.each(function(dayOption) {
+                if (dayOption.get('value') == day) {
+                    assert.isTrue(dayOption.get('selected'), 'because day is ' + day + ', day option with value of ' + dayOption.get('value') + ' should be selected');
+                } else {
+                    assert.isFalse(dayOption.get('selected'), 'because day is ' + day + ', day option with value of ' + dayOption.get('value') + ' should not be selected');
+                }
+            });
+            // year
+            assert.areEqual('1978', Y.one('input#birth-year').get('value'), 'birth year was not correct');
+        },
+        
+        testBindTextarea: function() {
+            var d = {bio:'This is my entire life story.'};
+            
+            Y.forms.FormBind.formBind(d, 'personal-info');
+            
+            assert.areEqual('This is my entire life story.', Y.one('#bio').get('value'));
+        },
+        
+        testBindTextarea_InDetailObject: function() {
+            var d = [{label: 'bio', data: 'This is my entire life story.'}];
+            
+            Y.forms.FormBind.formBind(d, 'personal-info');
+            
+            assert.areEqual('This is my entire life story.', Y.one('#bio').get('value'));
+        }
+        
+
+    }));
+    
+    Y.Test.Runner.add(testSuite);
+    
+}, "1.0");
